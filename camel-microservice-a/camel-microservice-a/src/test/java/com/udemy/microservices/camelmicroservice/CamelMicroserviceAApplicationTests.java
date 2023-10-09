60207a7a4c9742f5e46964138650222ba8fa59d4
alexander1914 <alexander.oliveira99@gmail.com>
2023-10-09 20:29:00 -0300

    Implementing first steps with apacha camel with routes

---
+++ camel-microservice-a/camel-microservice-a/src/test/java/com/udemy/microservices/camelmicroservice/CamelMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.microservices.camelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
